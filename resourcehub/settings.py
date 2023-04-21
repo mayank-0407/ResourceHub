82f014efa1e438bdc0820ec84e9c69c12df1b562
Mayank Aggarwal <97796657+mayank-0407@users.noreply.github.com>
2023-04-21 20:46:57 +0530

    removing credentials

--- resourcehub/settings.py
+++ resourcehub/settings.py
@@ -146,8 +146,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "dev.codebreakers@gmail.com"
-EMAIL_HOST_PASSWORD = "nvmjzbanoynjprmz"
+EMAIL_HOST_USER = "#"
+EMAIL_HOST_PASSWORD = "#"
 
 USER_STUDENT_CODE=121
 USER_STUDENT_NAME="student"
